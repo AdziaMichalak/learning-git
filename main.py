e741fa2383632c00855795bd2e99caf3209ff726
Magdalena Michalak <mbawolik80@gmail.com>
2022-05-02 09:55:39 +0200

    Add main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+print("lista zakupów".capitalize())
+lista = {
+    "piekarnia".capitalize(): ["chleb".capitalize(), "bułki".capitalize(), "pączek".capitalize()],
+    "warzywniak".capitalize(): ["marchew".capitalize(), "seler".capitalize(), "rukola".capitalize()],
+}
+
+for sklep, rzeczy in lista.items():
+    print("idę do %s i kupuję tam: %s".capitalize() % (sklep, rzeczy))
+   
+lista1 = lista ["Piekarnia"]
+lista2 = lista ["Warzywniak"]
+print(f"w sumie kupuję {len(lista1) + len(lista2)} produktów".capitalize())
